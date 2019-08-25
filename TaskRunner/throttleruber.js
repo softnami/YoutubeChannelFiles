e58db252b42dd680b006d1361f3f4956fd6152ea
Hussain <miralihussainkhan@gmail.com>
2019-08-25 11:34:19 -0400

    Added missing code.

--- TaskRunner/throttleruber.js
+++ TaskRunner/throttleruber.js
@@ -1,3 +1,38 @@
+class TaskThrottling {
+  constructor(maxCount) {
+    this.maxCount = maxCount;
+    this.taskQueue = [];
+    this.taskCount = 1;
+  }
+
+  pushArray(taskArray){
+      if(taskArray && Array.isArray(taskArray)){
+        for(let i =0; i<taskArray.length; i++){
+            this.push(taskArray[i]);
+        }
+      }
+  }
+
+  push(task) {
+    this.taskQueue.push(task);
+    this.run();
+  }
+
+  run() {
+    if (this.taskCount <= this.maxCount) {
+      if (this.taskQueue.length > 0) {
+        let task = this.taskQueue.shift();
+        this.taskCount++;
+        task(()=>{
+          this.taskCount--;
+          this.run();
+        });
+      }
+    }
+  }
+}
+
+
 let taskArray = [];
 let createTaskArray = ()=>{
     for(let i=0; i<10; i++){
